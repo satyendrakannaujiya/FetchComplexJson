5f5672c140eed3c0339dabe00b06b9980725e086
satyendrakannaujiya <satyendra250896@gmail.com>
2018-01-14 12:11:56 +0530

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { Http, Headers } from "@angular/http";
+import "rxjs/add/operator/map";
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  users: any;
+  constructor(public navCtrl: NavController, private http: Http) {}
+  loadjson() {
+    this.http
+      .get("https://randomuser.me/api/?results=10")
+      .map(val => {
+        val = val.json();
+        console.log("map  " + val);
+      })
+      .subscribe(
+        res => {
+          console.log("subscribe " + res);
+          this.users = res;
+        },
+        err => {
+          console.log("failed");
+        }
+      );
+    console.log(this.users);
   }
-
 }
